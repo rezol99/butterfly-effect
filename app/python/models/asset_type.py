35547d21cae0988fac88900888e847c42a908c90
Ryota Iizuka <ez.ryow@gmail.com>
2023-06-18 10:45:35 +0900

    chore: 不要なファイルを削除

--- app/python/models/asset_type.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class AssetType(Enum):
-    IMAGE = "image"
-    VIDEO = "video"
-    AUDIO = "audio"
--- app/python/models/timing.py
+++
@@ -1,7 +0,0 @@
-class Timing:
-    start: float
-    end: float
-
-    def __init__(self, start: float, end: float):
-        self.start = start
-        self.end = end
