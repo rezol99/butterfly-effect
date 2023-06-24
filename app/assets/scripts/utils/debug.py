ec42f32ded50c675e69a61e0fbb03cffba0bcee8
Ryota Iizuka <ez.ryow@gmail.com>
2023-06-24 22:30:12 +0900

    feat: デバッグ関数の引数を可変長にして赤字で表示するように変更

--- app/assets/scripts/utils/debug.py
+++ app/assets/scripts/utils/debug.py
@@ -1,4 +1,5 @@
 import sys
 
-def print_debug(message):
-    print(message, file=sys.stderr)
+def print_debug(*message):
+    message = " ".join([str(m) for m in message])
+    print('\033[31m'+message+'\033[0m', file=sys.stderr)
